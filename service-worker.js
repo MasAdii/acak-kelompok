0997f7fd30bff7d8de66b6d1dd6c3eb34cb6cdf9
masadii <githubmasadi@gmail.com>
2025-05-11 07:42:46 +0700

    problem

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "acak-kelompok-v1"
+const CACHE_NAME = "acak-kelompok-v1";
 const urlsToCache = [
   "index.html",
   "templates/contact.html",
@@ -12,41 +12,18 @@ const urlsToCache = [
   "assest/js/content.js",
   "assest/icons/icon-192.png",
   "assest/icons/icon-512.png"
-]
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache)
-    })
-  )
-})
-
-self.addEventListener("activate", event => {
-  const cacheWhitelist = [CACHE_NAME]
-
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName)
-          }
-        })
-      )
-    })
-  )
-})
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(networkResponse => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, networkResponse.clone())
-          return networkResponse
-        })
-      })
+      return response || fetch(event.request);
     })
-  )
-})
+  );
+});
